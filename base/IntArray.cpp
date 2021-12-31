636e307956ab832a09a33b4c7831c0194645ce44
Nick-sib <58276863+Nick-sib@users.noreply.github.com>
2021-12-31 20:19:34 +0700

    extend features
    added functionality Add findFirst, finfLast, creaty by copy

--- base/IntArray.cpp
+++ base/IntArray.cpp
@@ -1,32 +1,70 @@
-#include "IntArray.h"
+#include <iostream>
 #include <cassert> // for assert()
+#include "IntArray.h"
 
-IntArray::IntArray(int length): m_length{ length }
+
+IntArray::IntArray(int length): _length(length)
 {
     assert(length >= 0);
     if (length > 0)
-    m_data = new int[length]{};
+    _data = new int[length]{};
+}
+
+IntArray::IntArray(IntArray& array): _length(array.getLength())
+{
+    assert(array.getLength() >= 0);
+    if (_length > 0)
+    {
+        _data = new int[_length];
+        std::copy(array._data, array._data + _length, _data);
+    }
 }
 
 IntArray::~IntArray()
 {
-    delete[] m_data;
+    delete[] _data;
     // we don't need to set m_data to null or m_length to 0 here, since the object will be destroyed immediately after this function anyway
 }
 
 void IntArray::erase()
 {
-    delete[] m_data;
+    delete[] _data;
     // We need to make sure we set m_data to nullptr here, otherwise it will
     // be left pointing at deallocated memory!
-    m_data = nullptr;
-    m_length = 0;
+    _data = nullptr;
+    _length = 0;
 }
 
 int& IntArray::operator[](int index)
 {
-    assert(index >= 0 && index < m_length);
-    return m_data[index];
+    return get(index);
+}
+
+int& IntArray::get(int index)
+{
+    assert(index >= 0 && index < _length);
+    return _data[index];
+}
+
+int IntArray::findFirst(int value)
+{
+    int result = -1;
+    for (int i = 0; i < _length; i++)
+     if (_data[i] == value) 
+         return result;
+    
+    return result;
+}
+
+
+int IntArray::findLast(int value)
+{
+    int result = -1;
+    for (int i = _length - 1; i > 0; i--)
+     if (_data[i] == value) 
+         return result;
+
+    return result;
 }
 
 void IntArray::reallocate(int newLength)
@@ -39,14 +77,14 @@ void IntArray::reallocate(int newLength)
         return;
 
     // Then we have to allocate new elements
-    m_data = new int[newLength];
-    m_length = newLength;
+    _data = new int[newLength];
+    _length = newLength;
 }
 
 void IntArray::resize(int newLength)
 {
     // if the array is already the right length, we're done
-    if (newLength == m_length)
+    if (newLength == _length)
         return;
 
     // If we are resizing to an empty array, do that and return
@@ -68,78 +106,78 @@ void IntArray::resize(int newLength)
     // Then we have to figure out how many elements to copy from the existing
     // array to the new array.  We want to copy as many elements as there are
     // in the smaller of the two arrays.
-    if (m_length > 0)
+    if (_length > 0)
     {
-        int elementsToCopy{ (newLength > m_length) ? m_length : newLength };
+        int elementsToCopy{ (newLength > _length) ? _length : newLength };
 
         // Now copy the elements one by one
         for (int index{ 0 }; index < elementsToCopy ; ++index)
-            data[index] = m_data[index];
+            data[index] = _data[index];
     }
 
     // Now we can delete the old array because we don't need it any more
-    delete[] m_data;
+    delete[] _data;
 
     // And use the new array instead!  Note that this simply makes m_data point
     // to the same address as the new array we dynamically allocated.  Because
     // data was dynamically allocated, it won't be destroyed when it goes out of scope.
-    m_data = data;
-    m_length = newLength;
+    _data = data;
+    _length = newLength;
 }
 
 void IntArray::insertBefore(int value, int index)
 {
     // Sanity check our index value
-    assert(index >= 0 && index <= m_length);
+    assert(index >= 0 && index <= _length);
 
     // First create a new array one element larger than the old array
-    int* data{ new int[m_length+1] };
+    int* data{ new int[_length+1] };
 
     // Copy all of the elements up to the index
     for (int before{ 0 }; before < index; ++before)
-        data [before] = m_data[before];
+        data [before] = _data[before];
 
     // Insert our new element into the new array
     data[index] = value;
 
     // Copy all of the values after the inserted element
-    for (int after{ index }; after < m_length; ++after)
-        data[after+1] = m_data[after];
+    for (int after{ index }; after < _length; ++after)
+        data[after+1] = _data[after];
 
     // Finally, delete the old array, and use the new array instead
-    delete[] m_data;
-    m_data = data;
-    ++m_length;
+    delete[] _data;
+    _data = data;
+    ++_length;
 }
 
 void IntArray::remove(int index)
 {
     // Sanity check our index value
-    assert(index >= 0 && index < m_length);
+    assert(index >= 0 && index < _length);
 
     // If we're removing the last element in the array, we can just erase the array and return early
-    if (m_length == 1)
+    if (_length == 1)
     {
         erase();
         return;
     }
 
     // First create a new array one element smaller than the old array
-    int* data{ new int[m_length-1] };
+    int* data{ new int[_length-1] };
 
     // Copy all of the elements up to the index
     for (int before{ 0 }; before  < index; ++before)
-        data[before] = m_data[before];
+        data[before] = _data[before];
 
     // Copy all of the values after the removed element
-    for (int after{ index+1 }; after < m_length; ++after)
-        data[after-1] = m_data[after];
+    for (int after{ index+1 }; after < _length; ++after)
+        data[after-1] = _data[after];
 
     // Finally, delete the old array, and use the new array instead
-    delete[] m_data;
-    m_data = data;
-    --m_length;
+    delete[] _data;
+    _data = data;
+    --_length;
 }
 
 void IntArray::insertAtBeginning(int value) { insertBefore(value, 0); }
-void IntArray::insertAtEnd(int value) { insertBefore(value, m_length); }
+void IntArray::insertAtEnd(int value) { insertBefore(value, _length); }
--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include "base/IntArray.h"
 
 using namespace std;
 
 
 int main() 
 {
+
+    IntArray* arr = new IntArray(10);
+    IntArray& l = *arr;
+
+    for (size_t i = 0; i < arr->getLength(); i++)
+    {
+        arr->get(i) = i;
+        cout << "i = " << arr->get(i) << ", ";
+    }
+
+    arr->Add(123);
+
+
+    
+    cout << "\n";
+
+    IntArray arr2(*arr);
+
+    for (int i = 0; i < arr->getLength(); i++)
+    {
+        cout << "i = " << arr2[i] << ", ";
+    }
+
+    cout << endl;
+    
+    delete arr;
 
     return 0;
 }
